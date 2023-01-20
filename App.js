d11c301e5798b866790c632a5da345ca4b1ee630
Adriel Lira <adrieldiasdossantoslira@hotmail.com>
2023-01-20 10:43:44 -0300

    Finalização do exemplo com ImagePicker

--- App.js
+++ App.js
@@ -1,30 +1,30 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Button, Image, View, Platform } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 
 export default function ImagePickerExample() {
-  const [image, setImage] = useState(null);
 
-  const pickImage = async () => {
-    // No permissions request is necessary for launching the image library
-    let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.All,
-      allowsEditing: true,
-      aspect: [16, 9],
-      quality: 0.5,
-    });
+  const [foto, setFoto] = useState();
 
-    console.log(result);
+const acessarCamera = async ()=>{
+                                   
 
-    if (!result.canceled) {
-      setImage(result.assets[0].uri);
-    }
-  };
+
+                                     // Carregar/Acionar a Câmera de forma assíncrona
+  const imagem = await ImagePicker.launchCameraAsync({
+    allowsEditing: true,
+    aspect: [16, 9],
+    quality: 0.5,
+  });
+
+  console.log(imagem);
+  setFoto(imagem.assets[0].uri)
+}
 
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Button title="Pick an image from camera roll" onPress={pickImage} />
-      {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
+      <Button title="Acessar câmera" onPress={acessarCamera} />
+      { foto && <Image source={ {uri: foto} } style={{width: 300, height: 200, marginTop: 10}}/> }
     </View>
   );
 }
